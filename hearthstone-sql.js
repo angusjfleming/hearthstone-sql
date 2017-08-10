a45ddca9653b34e1a1bf3b4927bc6bf1959efcbf
Gus <gusmagician@gmail.com>
2017-08-10 14:56:52 +0100

    Updated for Knights, also no more conflicts

--- hearthstone-sql.js
+++ hearthstone-sql.js
@@ -3,7 +3,7 @@ const bingobongo = require("sqlite")
 const fs = require("fs")
 const unirest = require('unirest')
 const path = require('path')
-var setarray = ['Basic', 'Classic', 'Promo', 'Hall of Fame', 'Naxxramas', 'Goblins vs Gnomes', 'Blackrock Mountain', 'The Grand Tournament', 'The League of Explorers', 'Whispers of the Old Gods', 'One Night in Karazhan', 'Mean Streets of Gadgetzan', 'Journey to Un\'Goro']
+var setarray = ['Basic', 'Classic', 'Promo', 'Hall of Fame', 'Naxxramas', 'Goblins vs Gnomes', 'Blackrock Mountain', 'The Grand Tournament', 'The League of Explorers', 'Whispers of the Old Gods', 'One Night in Karazhan', 'Mean Streets of Gadgetzan', 'Journey to Un\'Goro', 'Knights of the Frozen Throne']
 
 exports.fetchdata = function(mashapekey) {
     return new Promise((resolve, reject) => {
@@ -21,31 +21,35 @@ exports.fetchdata = function(mashapekey) {
 
 exports.importcards = function() {
     return new Promise((resolve, reject) => {
-    bingobongo.open(__dirname + "\\carddb.sqlite", { Promise }).then(() => {    
-    var cardinfo = JSON.parse(fs.readFileSync(__dirname + '\\cardinfo.json' , 'utf8'))
-    bingobongo.run(`
+        bingobongo.open(__dirname + "\\carddb.sqlite", {
+            Promise
+        }).then(hssqlcarddb => {
+            hssqlcarddb.run("ATTACH (?) as carddb", `${__dirname}\\carddb.sqlite`)
+            var cardinfo = JSON.parse(fs.readFileSync(__dirname + '\\cardinfo.json', 'utf8'))
+            hssqlcarddb.run(`
     CREATE TABLE IF NOT EXISTS cardinfo ( cardID TEXT, name TEXT, cardSet TEXT, type TEXT, cardText TEXT, playerClass TEXT, locale TEXT, mechanics TEXT, faction TEXT, attack INTEGER, health INTEGER, img TEXT, imgGold TEXT, cost INTEGER, rarity TEXT, flavor TEXT, artist TEXT, collectible INTEGER, race TEXT, durability INTEGER, elite INTEGER, howToGet TEXT, howToGetGold TEXT, PRIMARY KEY(cardID) )
     `).then(() => {
-        bingobongo.run("DELETE FROM cardinfo")
-        var itemsProcessed = 0;
-        
-            setarray.forEach(function(set) {
-                cardinfo[`${set}`].forEach(function(element) {
-                    bingobongo.run("INSERT INTO cardinfo (cardID, name, cardSet, type, cardText, playerClass, locale, mechanics, faction, attack, health, img, imgGold, cost, rarity, flavor, artist, collectible, race, durability, elite, howToGet, howToGetGold) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [element.cardId, element.name, element.cardSet, element.type, element.text, element.playerClass, element.locale, JSON.stringify(element.mechanics), element.faction, element.attack, element.health, element.img, element.imgGold, element.cost, element.rarity, element.flavor, element.artist, element.collectible, element.race, element.durability, element.elite, element.howToGet, element.howToGetGold]).then(() => {
-                            if (set == setarray[setarray.length - 1] && cardinfo[set][cardinfo[set].length - 1].cardId == element.cardId) {
-                                resolve(true)
-                                bingobongo.close()
-                            }
-                        })
-                        .catch((err) => {
-                            reject(err)
-                        });
+                hssqlcarddb.run("DELETE FROM cardinfo")
+                var itemsProcessed = 0;
+
+                setarray.forEach(function(set) {
+                    cardinfo[`${set}`].forEach(function(element) {
+                        hssqlcarddb.run("INSERT INTO cardinfo (cardID, name, cardSet, type, cardText, playerClass, locale, mechanics, faction, attack, health, img, imgGold, cost, rarity, flavor, artist, collectible, race, durability, elite, howToGet, howToGetGold) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [element.cardId, element.name, element.cardSet, element.type, element.text, element.playerClass, element.locale, JSON.stringify(element.mechanics), element.faction, element.attack, element.health, element.img, element.imgGold, element.cost, element.rarity, element.flavor, element.artist, element.collectible, element.race, element.durability, element.elite, element.howToGet, element.howToGetGold]).then(() => {
+                                if (set == setarray[setarray.length - 1] && cardinfo[set][cardinfo[set].length - 1].cardId == element.cardId) {
+                                    hssqlcarddb.close()
+                                    resolve(true)
+                                }
+                            })
+                            .catch((err) => {
+                                hssqlcarddb.close()
+                                reject(err)
+                            });
+                    })
                 })
+
             })
-        
+        })
     })
-    })
-})
 }
 exports.dbpath = __dirname + "\\carddb.sqlite"
 exports.jsonpath = __dirname + "\\cardinfo.json"
